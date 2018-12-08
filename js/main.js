cb71f1ca13135b999983a96bef82fa42257bfb47
Abhihek Kumar <kumarabhishek.kumkar97@gmail.com>
2018-12-08 19:04:20 +0530

    Final_html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('.login-btn-model').click(function(){
+    $('.login').modal();
+});
+
+$('.register-btn-model').click(function(){
+  $('.register').modal();
+});
+
+
+
+});
